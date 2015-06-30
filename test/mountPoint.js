2ee0b11ad0cdaf286b467fd64e9dde991d55d106
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-30 19:56:07 +0000

    Add mountpoint tests.

---
+++ test/mountPoint.js
@@ -0,0 +1,23 @@
+/*jslint node: true */
+"use strict";
+
+
+var dualproto = require('dual-protocol');
+
+describe('dual-broadcast', function () {
+    
+    var d;
+    beforeEach(function () {
+        d = (dualproto.use(require('../index')))()
+    });
+
+    it('should listen on "broadcast" by default', function () {
+        d.broadcast();
+        assert.notEqual(0, d.listeners(['broadcast', 'subscribe', '**']).length);
+    });
+
+    it('should listen on provided route if provided', function () {
+        d.broadcast(['c']);
+        assert.notEqual(0, d.listeners(['c', 'subscribe', '**']).length);
+    });
+});
