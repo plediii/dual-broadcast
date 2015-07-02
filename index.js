b2094c039d6c929dbb24206dccc6b0a27fe5adb2
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-07-02 04:44:54 +0000

    Satisfy existing tests.

--- index.js
+++ index.js
@@ -4,30 +4,52 @@
 module.exports = function (Domain, libs) {
     var _ = libs._;
     Domain.prototype.broadcast = function (route) {
+        if (!_.isArray(route)) {
+            route = ['broadcast'];
+        }
         var d = this;
         var subscriptions = new Domain();
         d.mount(route, {
-            subscribe: {
-                '::subscriber': function (body, ctxt) {
-                    var subscriber = ctxt.params.subscriber;
-                    var subscription = ctxt.from;
-                    console.log('mounting subscription ', subscription);
-                    subscriptions.mount(subscription, function (body, ctxt) {
-                        console.log('broadcasting ', ctxt.from, ' -> ', subscription);
-                        d.send(_.defaults({
-                            to: subscriber
-                        }, ctxt));
+            register: {
+                '::client': function (body, ctxt) {
+                    var client = ctxt.params.client;
+                    var unsubscribeRoute = route.concat('unsubscribe').concat(client);
+                    var subscribeRoute = route.concat('subscribe').concat(client);
+                    var transfer = function (body, ctxt) {
+                        d.send({
+                            to: client
+                            , from: ctxt.from
+                            , body: body
+                            , options: ctxt.options
+                        });
+                    };
+                    d.mount(subscribeRoute, function (body, ctxt) {
+                        var subscription = ctxt.from;
+                        subscriptions.mount(subscription, transfer);
+                        d.send(route.concat(['newListener']), subscription);
+                        d.send(route.concat(['newListener'].concat(client)), subscription);
+                    });
+                    d.mount(unsubscribeRoute, function (body, ctxt) {
+                        var subscription = ctxt.from;
+                        subscriptions.removeListener(ctxt.from, transfer);
+                        d.send(route.concat(['removeListener']), subscription);
+                        d.send(route.concat(['removeListener'].concat(client)), subscription);
+                    });
+                    d.once(['disconnect'].concat(client), function () {
+                        d.send(unsubscribeRoute, ['**']);
+                        d.unmount(subscribeRoute);
+                        d.unmount(unsubscribeRoute);
                     });
                 }
             }
-            , unsubscribe: {
-                '::subscriber': function (body, ctxt) {
-                    
-                }
-            }
             , send: function (body, ctxt) {
-                console.log('sending ', ctxt.from);
-                subscriptions.send(ctxt.from);
+                var subscription = ctxt.from;
+                subscriptions.send({
+                    to: subscription
+                    , from: subscription
+                    , body: body
+                    , options: ctxt.options
+                });
             }
         });
     };
