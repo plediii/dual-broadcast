6d9bca71b2196879824ed79ec500bf56462e1ee5
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-07-02 22:30:40 +0000

    Optimization trial.

--- index.js
+++ index.js
@@ -13,30 +13,35 @@ module.exports = function (Domain, libs) {
             register: {
                 '::client': function (body, ctxt) {
                     var client = ctxt.params.client;
+                    var unsubscriptions = new Domain();
                     var unsubscribeRoute = route.concat('unsubscribe').concat(client);
                     var subscribeRoute = route.concat('subscribe').concat(client);
-                    var transfer = function (body, ctxt) {
-                        d.send({
-                            to: client
-                            , from: ctxt.from
-                            , body: body
-                            , options: ctxt.options
-                        });
-                    };
                     d.mount(subscribeRoute, function (body, ctxt) {
                         var subscription = ctxt.from;
+                        var transfer = function (body, ctxt) {
+                            d.send({
+                                to: client
+                                , from: ctxt.from
+                                , body: body
+                                , options: ctxt.options
+                            });
+                        };
+
                         subscriptions.mount(subscription, transfer);
+                        unsubscriptions.once(subscription, function (body, ctxt) {
+                            subscriptions.removeListener(subscription, transfer);
+                        });
                         d.send(route.concat(['newListener']), subscription);
                         d.send(route.concat(['newListener'].concat(client)), subscription);
                     });
                     d.mount(unsubscribeRoute, function (body, ctxt) {
                         var subscription = ctxt.from;
-                        subscriptions.removeListener(ctxt.from, transfer);
+                        unsubscriptions.send(subscription);
                         d.send(route.concat(['removeListener']), subscription);
                         d.send(route.concat(['removeListener'].concat(client)), subscription);
                     });
                     d.once(['disconnect'].concat(client), function () {
-                        d.send(unsubscribeRoute, ['**']);
+                        unsubscriptions.send(['**']);
                         d.unmount(subscribeRoute);
                         d.unmount(unsubscribeRoute);
                     });
