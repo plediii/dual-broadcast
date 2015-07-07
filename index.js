e83d5cfb9d3c934f97677fac23ee7ea9ad4bf8ad
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-07-07 14:14:24 +0000

    Satisfy existing tests.

--- index.js
+++ index.js
@@ -13,9 +13,20 @@ module.exports = function (Domain, libs) {
             register: {
                 '::client': function (body, ctxt) {
                     var client = ctxt.params.client;
-                    var unsubscriptions = new Domain();
+                    var clientSubscriptions = new Domain();
                     var unsubscribeRoute = route.concat('unsubscribe').concat(client);
                     var subscribeRoute = route.concat('subscribe').concat(client);
+
+                    var unsubscribe = function (subscription) {
+                        clientSubscriptions.removeAllListeners(subscription);
+                    };
+
+                    clientSubscriptions.on(['removeListener'], function (subscription, f) {
+                        subscriptions.removeListener(subscription, f);
+                        d.send(route.concat(['removeListener']), subscription);
+                        d.send(route.concat(['removeListener'].concat(client)), subscription);
+                    });
+
                     d.mount(subscribeRoute, function (body, ctxt) {
                         var subscription = ctxt.from;
                         var transfer = function (body, ctxt) {
@@ -28,22 +39,18 @@ module.exports = function (Domain, libs) {
                         };
 
                         subscriptions.mount(subscription, transfer);
-                        unsubscriptions.once(subscription, function (body, ctxt) {
-                            subscriptions.removeListener(subscription, transfer);
-                        });
+                        clientSubscriptions.on(subscription, transfer);
                         d.send(route.concat(['newListener']), subscription);
                         d.send(route.concat(['newListener'].concat(client)), subscription);
                     });
                     d.mount(unsubscribeRoute, function (body, ctxt) {
                         var subscription = ctxt.from;
-                        unsubscriptions.send(subscription);
-                        d.send(route.concat(['removeListener']), subscription);
-                        d.send(route.concat(['removeListener'].concat(client)), subscription);
+                        unsubscribe(subscription);
                     });
                     d.once(['disconnect'].concat(client), function () {
-                        unsubscriptions.send(['**']);
                         d.unmount(subscribeRoute);
                         d.unmount(unsubscribeRoute);
+                        unsubscribe('**');
                     });
                 }
             }
